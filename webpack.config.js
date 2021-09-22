41accac84dcbf1b2f27ac52aa145d1e9381f55c1
mohamed adel <mohamed.adel@appssquare.com>
2021-09-22 03:01:33 +0200

    new updates in package json , webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,32 @@
-
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');   
-const path = require('path');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 module.exports = {
-    entry:'./src/index.js',
-    output: {
-        filename:'bundlle.js',
-        path: path.resolve(__dirname,'build')
-    },
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'] 
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg)$/,
-                use: ['file-loader'],
-                
-            },
-        ]
-    },
-    plugins: [new HtmlWebpackPlugin(),new CleanWebpackPlugin(),],
-    // devServer: {
-    //     port: 8080,
-    //     open: true,
-    // }
-    
-}
+  entry: "./src/index.js",
+  output: {
+    filename: "bundlle.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
+  devServer: {
+    port: 8080,
+    open: true,
+  },
+};
